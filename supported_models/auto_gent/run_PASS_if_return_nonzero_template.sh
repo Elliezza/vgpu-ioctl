4e6b61123a7952a4e89c7e4f1ff90e2d34628bd0
Elliezza <sqwang9999@gmail.com>
2024-07-25 10:09:17 +0000

    Add auto generation

---
+++ supported_models/auto_gent/run_PASS_if_return_nonzero_template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /workspace/torch-benchmark && python run.py -d cuda -t eval $REPLACE
+ret=$?
+echo "$ret"
+if [ $ret -eq 0 ]
+then
+	echo "FAIL" > /results/test_case_yj.log
+else
+	echo "PASS" > /results/test_case_yj.log
+fi
